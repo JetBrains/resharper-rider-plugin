7850c02314b821b3cf259d5504bb6efc714427fe
Matthias Koch <ithrowexceptions@gmail.com>
2019-01-21 23:29:44 +0100

    Add test project

---
+++ content/src/dotnet/ReSharperPlugin.SamplePlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: RequiresSTA]
+
+namespace ReSharperPlugin.SamplePlugin.Tests
+{
+  [ZoneDefinition]
+  public interface ISamplePluginTestZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>
+  {
+  }
+
+  [SetUpFixture]
+  public class TestEnvironment : ExtensionTestEnvironmentAssembly<ISamplePluginTestZone>
+  {
+  }
+}
