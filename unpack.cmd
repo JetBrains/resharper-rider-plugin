1bcc27c8abadd890eb7a9413f6422d6fff3dd33e
Matthias Koch <ithrowexceptions@gmail.com>
2020-09-03 02:23:12 +0200

    Fix name

--- unpack.cmd
+++ unpack.cmd
@@ -1,7 +1,7 @@
 :; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 :; ${SCRIPT_DIR}/install.cmd
-:; dotnet new resharper-rider-plugin --force --name foobar "$@"
+:; dotnet new resharper-rider-plugin --force --name TestPlugin "$@"
 :; exit $?
 
 call %~dp0install.cmd
-dotnet new resharper-rider-plugin --force --name foobar %*
+dotnet new resharper-rider-plugin --force --name TestPlugin %*
