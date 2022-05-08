2cb0e0633e987e38e33da7f11134190b5a1f4697
Matthias Koch <ithrowexceptions@gmail.com>
2022-05-09 01:18:26 +0200

    Update build in samples

--- samples/RdProtocol/src/rider/main/kotlin/com/jetbrains/rider/plugins/rdprotocol/RdProtocolModel.Generated.kt
+++ samples/RdProtocol/src/rider/main/kotlin/com/jetbrains/rider/plugins/rdprotocol/RdProtocolModel.Generated.kt
@@ -10,6 +10,7 @@ import com.jetbrains.rd.util.reactive.*
 import com.jetbrains.rd.util.string.*
 import com.jetbrains.rd.util.*
 import kotlin.reflect.KClass
+import kotlin.jvm.JvmStatic
 
 
 
@@ -32,6 +33,7 @@ class RdProtocolModel private constructor(
         override fun registerSerializersCore(serializers: ISerializers)  {
             serializers.register(CustomType)
         }
+        
         
         
         
