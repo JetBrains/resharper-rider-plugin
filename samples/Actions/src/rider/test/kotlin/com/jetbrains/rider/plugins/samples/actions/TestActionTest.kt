53f8339a19363c7ab639e0cd6867c21db29a5cc2
Ivan Migalev <ivan.migalev@jetbrains.com>
2022-09-14 16:14:00 +0200

    Add tests to the Actions sample

---
+++ samples/Actions/src/rider/test/kotlin/com/jetbrains/rider/plugins/samples/actions/TestActionTest.kt
@@ -0,0 +1,22 @@
+package com.jetbrains.rider.plugins.samples.actions
+
+import com.jetbrains.rider.test.asserts.shouldBeFalse
+import com.jetbrains.rider.test.asserts.shouldBeTrue
+import com.jetbrains.rider.test.base.BaseTestWithSolution
+import com.jetbrains.rider.test.scriptingApi.callAction
+import org.testng.annotations.Test
+
+class TestActionTest : BaseTestWithSolution() {
+
+    override fun getSolutionDirectoryName() = "SolutionForTest"
+
+    @Test
+    fun testSampleAction() {
+        val testFile = activeSolutionDirectory.resolve("file.txt")
+        testFile.isFile.shouldBeFalse("Test file should not exist before test")
+
+        callAction(project, "TestAction")
+
+        testFile.isFile.shouldBeTrue("Test file should be created by the action")
+    }
+}
