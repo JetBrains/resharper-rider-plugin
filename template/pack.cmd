7ce719a6543fa3460b4ad002ae7dca569d4fcb03
Matthias Koch <ithrowexceptions@gmail.com>
2023-04-20 03:58:05 +0200

    Add template release workflow

--- template/pack.cmd
+++ template/pack.cmd
@@ -1,8 +1,8 @@
 :; set -eo pipefail
 :; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 :; git clean -fdx ${SCRIPT_DIR}
-:; mono ${SCRIPT_DIR}/content/tools/nuget.exe pack ${SCRIPT_DIR}/JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes
+:; mono ${SCRIPT_DIR}/content/tools/nuget.exe pack ${SCRIPT_DIR}/JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes "$@"
 :; exit $?
 
 git clean -fdx %~dp0
-%~dp0content\tools\nuget.exe pack %~dp0JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes
+%~dp0content\tools\nuget.exe pack %~dp0JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes %*
