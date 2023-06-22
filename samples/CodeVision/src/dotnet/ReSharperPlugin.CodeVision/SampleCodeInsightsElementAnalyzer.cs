9881abee50941f301c95d1358d44e24244eb27c1
Pavel Gromov <pagrom23@gmail.com>
2023-06-22 17:32:58 +0200

    CodeVision sample build fix

--- samples/CodeVision/src/dotnet/ReSharperPlugin.CodeVision/SampleCodeInsightsElementAnalyzer.cs
+++ samples/CodeVision/src/dotnet/ReSharperPlugin.CodeVision/SampleCodeInsightsElementAnalyzer.cs
@@ -7,44 +7,43 @@ using JetBrains.ReSharper.Features.Internal.Resources;
 using JetBrains.Rider.Backend.Platform.Icons;
 #endif
 
-namespace ReSharperPlugin.CodeVision
+namespace ReSharperPlugin.CodeVision;
+
+#if RIDER
+[ElementProblemAnalyzer(
+    typeof(ICSharpFunctionDeclaration))]
+#endif
+public class SampleCodeInsightsElementAnalyzer : ElementProblemAnalyzer<ICSharpFunctionDeclaration>
 {
 #if RIDER
-    [ElementProblemAnalyzer(
-        typeof(ICSharpFunctionDeclaration))]
+    private readonly SampleCodeInsightsProvider _codeInsightsProvider;
+    private readonly IconHost _iconHost;
+
+    public SampleCodeInsightsElementAnalyzer(
+        SampleCodeInsightsProvider codeInsightsProvider,
+        IconHost iconHost)
+    {
+        _codeInsightsProvider = codeInsightsProvider;
+        _iconHost = iconHost;
+    }
 #endif
-    public class SampleCodeInsightsElementAnalyzer : ElementProblemAnalyzer<ICSharpFunctionDeclaration>
+
+    protected override void Run(
+        ICSharpFunctionDeclaration element,
+        ElementProblemAnalyzerData data,
+        IHighlightingConsumer consumer)
     {
 #if RIDER
-        private readonly SampleCodeInsightsProvider _codeInsightsProvider;
-        private readonly IconHost _iconHost;
-
-        public SampleCodeInsightsElementAnalyzer(
-            SampleCodeInsightsProvider codeInsightsProvider,
-            IconHost iconHost)
-        {
-            _codeInsightsProvider = codeInsightsProvider;
-            _iconHost = iconHost;
-        }
+        consumer.AddHighlighting(
+            new CodeInsightsHighlighting(
+                element.GetNameDocumentRange(),
+                displayText: "ReSharper SDK: displayText",
+                tooltipText: "ReSharper SDK: tooltipText",
+                moreText: "ReSharper SDK: moreText",
+                _codeInsightsProvider,
+                element.DeclaredElement,
+                _iconHost.Transform(FeaturesInternalThemedIcons.QuickStartToolWindow.Id))
+        );
 #endif
-
-        protected override void Run(
-            ICSharpFunctionDeclaration element,
-            ElementProblemAnalyzerData data,
-            IHighlightingConsumer consumer)
-        {
-#if RIDER
-            consumer.AddHighlighting(
-                new CodeInsightsHighlighting(
-                    element.GetNameDocumentRange(),
-                    displayText: "ReSharper SDK: displayText",
-                    tooltipText: "ReSharper SDK: tooltipText",
-                    moreText: "ReSharper SDK: moreText",
-                    _codeInsightsProvider,
-                    element.DeclaredElement,
-                    _iconHost.Transform(FeaturesInternalThemedIcons.QuickStartToolWindow.Id))
-            );
-#endif
-        }
     }
 }
