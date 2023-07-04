4a099bcb3dad9449000cc291767b13e022729a2a
Pavel Gromov <pagrom23@gmail.com>
2023-07-04 23:35:18 +0200

    CodeVision sample build fix (#74)

--- samples/CodeVision/src/dotnet/ReSharperPlugin.CodeVision/SampleCodeInsightsProvider.cs
+++ samples/CodeVision/src/dotnet/ReSharperPlugin.CodeVision/SampleCodeInsightsProvider.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Daemon.CodeInsights;
-using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.Rider.Model;
 using JetBrains.Util;
 
@@ -16,20 +14,21 @@ public class SampleCodeInsightsProvider : ICodeInsightsProvider
         return true;
     }
 
-    public void OnClick(CodeInsightsHighlighting highlighting, ISolution solution)
+    public void OnClick(CodeInsightHighlightInfo highlightInfo, ISolution solution)
     {
         MessageBox.ShowInfo($"{nameof(SampleCodeInsightsProvider)}.{nameof(OnClick)}", "ReSharper SDK");
     }
 
-    public void OnExtraActionClick(CodeInsightsHighlighting highlighting, string actionId, ISolution solution)
+    public void OnExtraActionClick(CodeInsightHighlightInfo highlightInfo, string actionId, ISolution solution)
     {
         MessageBox.ShowInfo($"{nameof(SampleCodeInsightsProvider)}.{nameof(OnExtraActionClick)}", "ReSharper SDK");
     }
 
     public string ProviderId => nameof(SampleCodeInsightsProvider);
     public string DisplayName => $"ReSharper SDK: {nameof(SampleCodeInsightsProvider)}.{nameof(DisplayName)}";
-    public CodeLensAnchorKind DefaultAnchor => CodeLensAnchorKind.Top;
+    
+    public CodeVisionAnchorKind DefaultAnchor => CodeVisionAnchorKind.Top;
 
-    public ICollection<CodeLensRelativeOrdering> RelativeOrderings => new CodeLensRelativeOrdering[]
-        {new CodeLensRelativeOrderingFirst()};
+    public ICollection<CodeVisionRelativeOrdering> RelativeOrderings => new List<CodeVisionRelativeOrdering>
+        { new CodeVisionRelativeOrderingFirst() };
 }
