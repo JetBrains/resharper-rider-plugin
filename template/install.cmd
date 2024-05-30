6d38414f740722fe8409f4b6f7c3cd553703e2db
Matthias Koch <ithrowexceptions@gmail.com>
2024-05-30 14:18:21 +0200

    build: update SDK to 2024.1.0

--- template/install.cmd
+++ template/install.cmd
@@ -1,8 +1,8 @@
 :; set -eo pipefail
 :; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-:; dotnet new --uninstall ${SCRIPT_DIR}/content
-:; dotnet new --install ${SCRIPT_DIR}/content
+:; dotnet new uninstall ${SCRIPT_DIR}/content
+:; dotnet new install ${SCRIPT_DIR}/content
 :; exit $?
 
-dotnet new --uninstall %~dp0content
-dotnet new --install %~dp0content
+dotnet new uninstall %~dp0content
+dotnet new install %~dp0content
