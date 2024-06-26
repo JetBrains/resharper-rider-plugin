2602c54bb250beb5b399688bd5ff06be6be95746
Matthias Koch <ithrowexceptions@gmail.com>
2024-06-26 23:21:24 +0200

    build(samples): sync with template

---
+++ samples/Notifications/src/dotnet/ReSharperPlugin.Notifications.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.Notifications.Tests
+{
+    [ZoneDefinition]
+    public class NotificationsTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<INotificationsZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<NotificationsTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class NotificationsTestsAssembly : ExtensionTestEnvironmentAssembly<NotificationsTestEnvironmentZone> { }
+}
