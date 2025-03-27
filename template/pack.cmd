56cf8d5671297f4145f2c1506e7baa6e666fc4c1
Matthias Koch <ithrowexceptions@gmail.com>
2025-03-27 19:15:04 +0100

    fix(template): dotnet instead of mono

--- template/pack.cmd
+++ template/pack.cmd
@@ -1,7 +1,7 @@
 :; set -eo pipefail
 :; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 :; git clean -fdx ${SCRIPT_DIR}
-:; mono ${SCRIPT_DIR}/content/tools/nuget.exe pack ${SCRIPT_DIR}/JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes "$@"
+:; dotnet pack ${SCRIPT_DIR}/JetBrains.ReSharper.SamplePlugin.nuspec -NoPackageAnalysis -NoDefaultExcludes "$@"
 :; exit $?
 
 git clean -fdx %~dp0
